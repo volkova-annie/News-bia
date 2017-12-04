eb77a51ec85a1e957556859154cd8c1b74d2b678
Volkova Annie <annie_v@bk.ru>
2017-12-04 18:33:22 +0300

    add router link

--- client/index.js
+++ client/index.js
@@ -6,11 +6,19 @@ import configureStore from './configure-store';
 import initialState from './initial-state';
 import {App} from '../components';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 const store = configureStore(initialState);
 
 let target = document.getElementById('root');
 
 ReactDOM.render(
-  <Provider store={store}><App/></Provider>,
+  <Provider store={store}>
+    <Router>
+        <Route path="/" component={App} />
+      </Router>
+  </Provider>,
   target
 );
+
+// <Route path="/post" component={Post}/>
